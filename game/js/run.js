870ca9476eb5478f99610534cbbe7de88ef3817a
Andrew <weblings.lings@gmail.com>
2017-05-05 00:44:29 -0700

    fixed merge conflicts between states and player. preloader borken

--- game/js/run.js
+++ game/js/run.js
@@ -3,8 +3,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
 
 //load up states from other files NH
-game.state.add('preloader', preloader);
 game.state.add('main', mainState);
+game.state.add('preloader', preloader);
 
 //start preload NH
 game.state.start('preloader');

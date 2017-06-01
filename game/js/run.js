04fe6aa884520d6decfc2a63a022ef6af2848456
Dendral W <Dendral W>
2017-05-31 23:30:17 -0700

    added title screen and credits page with proper routes between; still need controls page added

--- game/js/run.js
+++ game/js/run.js
@@ -4,6 +4,8 @@ var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'gameDiv');
 
 //load up states from other files NH
 game.state.add('preloader', preloaderState);
+game.state.add('title', titleState);
+game.state.add('credits', creditState);
 game.state.add('main', mainState);
 game.state.add('charSelect',charSelect);
 

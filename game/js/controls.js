6b62e7395d7e2dd584286c8ebcaadc24bbf410af
Evie Chang <evchang@ucsc.edu>
2017-06-01 23:29:36 -0700

    added controls state and CharSelect UI

--- game/js/controls.js
+++ game/js/controls.js
@@ -2,6 +2,61 @@ var controlState = {
 
     
     create: function(){
+		//PLAYER 1 SELECT
+		var layoutSelect = game.add.sprite(70, 40, "selectBox");
+		
+		var jumpSelect1 = game.add.sprite(40, 410, "selectBox2");
+		jumpSelect1.scale.setTo(.23, .14);
+		jumpSelect1.alpha = 0;
+		
+		var leftSelect1 = game.add.sprite(40, 455, "selectBox2");
+		leftSelect1.scale.setTo(.21, .14);
+		leftSelect1.alpha = 0;
+		
+		var rightSelect1 = game.add.sprite(40, 500, "selectBox2");
+		rightSelect1.scale.setTo(.24, .14);
+		rightSelect1.alpha = 0;
+		
+		var guardSelect1 = game.add.sprite(40, 545, "selectBox2");
+		guardSelect1.scale.setTo(.28, .14);
+		guardSelect1.alpha = 0;
+		
+		var lightAttackSelect1 = game.add.sprite(38, 590, "selectBox2");
+		lightAttackSelect1.scale.setTo(.52, .14);
+		lightAttackSelect1.alpha = 0;
+		
+		var heavyAttackSelect1 = game.add.sprite(28, 635, "selectBox2");
+		heavyAttackSelect1.scale.setTo(.9, .14);
+		heavyAttackSelect1.alpha = 0;
+		
+		//PLAYER 2 SELECT
+		var layoutSelect2 = game.add.sprite(720, 40, "selectBox");
+		
+		var jumpSelect2 = game.add.sprite(720, 410, "selectBox2");
+		jumpSelect2.scale.setTo(.23, .14);
+		jumpSelect2.alpha = 0;
+		
+		var leftSelect2 = game.add.sprite(720, 455, "selectBox2");
+		leftSelect2.scale.setTo(.21, .14);
+		leftSelect2.alpha = 0;
+		
+		var rightSelect2 = game.add.sprite(720, 500, "selectBox2");
+		rightSelect2.scale.setTo(.24, .14);
+		rightSelect2.alpha = 0;
+		
+		var guardSelect2 = game.add.sprite(720, 545, "selectBox2");
+		guardSelect2.scale.setTo(.28, .14);
+		guardSelect2.alpha = 0;
+		
+		var lightAttackSelect2 = game.add.sprite(710, 590, "selectBox2");
+		lightAttackSelect2.scale.setTo(.52, .14);
+		lightAttackSelect2.alpha = 0;
+		
+		var heavyAttackSelect2 = game.add.sprite(700, 635, "selectBox2");
+		heavyAttackSelect2.scale.setTo(.9, .14);
+		heavyAttackSelect2.alpha = 0;
+		
+		
        var controls1 = game.add.sprite(105,60,"control1");
 	   controls1.scale.setTo(.75,.75);
 	   controls1.alpha = 0;
@@ -14,6 +69,8 @@ var controlState = {
 	   
 	   sprites = [controls1, controls2];
 	   sprites2 = [controls1_2, controls2_2];
+	   selectBoxes = [layoutSelect, jumpSelect1, leftSelect1, rightSelect1, guardSelect1, lightAttackSelect1, heavyAttackSelect1];
+	   selectBoxes2 = [layoutSelect2, jumpSelect2, leftSelect2, rightSelect2, guardSelect2, lightAttackSelect2, heavyAttackSelect2];
 	   
 	   preset1_1 = ['W', 'A', 'S', 'D', 'E', 'Q'];
 	   preset1_2 = ['W', 'A', 'S', 'D', 'E', 'R'];
@@ -56,53 +113,53 @@ var controlState = {
     
     update: function(){
         //P1 keys
-        if(game.input.keyboard.justPressed(P1keyUp) && !P1Chose){
-            if(P1index + 2 > sprites.length){
-               sprites[P1index].alpha = 0;
+        if(game.input.keyboard.justPressed(P1keyDown)){
+            if(P1index + 2 > selectBoxes.length){
+               selectBoxes[P1index].alpha = 0;
 			   P1index = 0;
-			   sprites[P1index].alpha = 1;
+			   selectBoxes[P1index].alpha = 1;
             }else{
-				sprites[P1index].alpha = 0;
+				selectBoxes[P1index].alpha = 0;
                 P1index++;
-				sprites[P1index].alpha = 1;
+				selectBoxes[P1index].alpha = 1;
             }
         }
         
-        if(game.input.keyboard.justPressed(P1keyDown) && !P1Chose){
+        if(game.input.keyboard.justPressed(P1keyUp)){
             if(P1index - 1 < 0){
-				sprites[P1index].alpha = 0;
-                P1index = characters.length-1;
-				sprites[P1index].alpha = 1;
+				selectBoxes[P1index].alpha = 0;
+                P1index = selectBoxes.length-1;
+				selectBoxes[P1index].alpha = 1;
             }else{
-				sprites[P1index].alpha = 0;
+				selectBoxes[P1index].alpha = 0;
                 P1index--;
-				sprites[P1index].alpha = 1;
+				selectBoxes[P1index].alpha = 1;
             }
         }
         
         
         //P2 keys
-        if(game.input.keyboard.justPressed(P2keyUp) && !P2Chose){
-            if(P2index + 2 > sprites2.length){
-				sprites2[P2index].alpha = 0;
+        if(game.input.keyboard.justPressed(P2keyDown)){
+            if(P2index + 2 > selectBoxes2.length){
+				selectBoxes2[P2index].alpha = 0;
                 P2index = 0;
-				sprites2[P2index].alpha = 1;
+				selectBoxes2[P2index].alpha = 1;
             }else{
-				sprites2[P2index].alpha = 0;
+				selectBoxes2[P2index].alpha = 0;
                 P2index++;
-				sprites2[P2index].alpha = 1;
+				selectBoxes2[P2index].alpha = 1;
             }
         }
         
-        if(game.input.keyboard.justPressed(P2keyDown) && !P2Chose){
+        if(game.input.keyboard.justPressed(P2keyUp)){
             if(P2index - 1 < 0){
-				sprites2[P2index].alpha = 0;
-                P2index = characters.length-1;
-				sprites2[P2index].alpha = 1;
+				selectBoxes2[P2index].alpha = 0;
+                P2index = selectBoxes2.length-1;
+				selectBoxes2[P2index].alpha = 1;
             }else{
-				sprites2[P2index].alpha = 0;
+				selectBoxes2[P2index].alpha = 0;
                 P2index--;
-				sprites2[P2index].alpha = 1;
+				selectBoxes2[P2index].alpha = 1;
             }
         }
         
